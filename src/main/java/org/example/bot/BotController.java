b52271d4193e8d691e1eed660b14adf06d2e1e2c
kostiantyn <sinkkostia@gmail.com>
2023-09-21 19:05:36 +0300

    test 17

--- src/main/java/org/example/bot/BotController.java
+++ src/main/java/org/example/bot/BotController.java
@@ -94,10 +94,10 @@ public class BotController {
                     }
 
 
-                    String userKeyAdmin = USER_DB_MAP_KEY + ":" + AdminID;
-                    Date adminDate = new Date();
-                    User adminUser2 = new User("Admin", "64", false, false, adminDate, adminDate, 1, true, true, true, 1, 50);
-                    jedis.set(userKeyAdmin, convertUserToJson(adminUser2));
+//                    String userKeyAdmin = USER_DB_MAP_KEY + ":" + AdminID;
+//                    Date adminDate = new Date();
+//                    User adminUser2 = new User("Admin", "64", false, false, adminDate, adminDate, 1, true, true, true, 1, 50);
+//                    jedis.set(userKeyAdmin, convertUserToJson(adminUser2));
 
 
                     try {
