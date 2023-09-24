98ec7f6cab9483220fdf0f445568c44f28b38542
kostiantyn <sinkkostia@gmail.com>
2023-09-24 17:07:29 +0300

    test 22

--- src/main/java/org/example/bot/DateUtil.java
+++ src/main/java/org/example/bot/DateUtil.java
@@ -1,5 +1,7 @@
 package org.example.bot;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.Date;
@@ -20,6 +22,11 @@ public class DateUtil {
     }
 
     public static boolean isWithinTradingHours(LocalTime currentTime) {
+
+        if (LocalDate.now().getDayOfWeek() == DayOfWeek.SATURDAY || LocalDate.now().getDayOfWeek() == DayOfWeek.SUNDAY) {
+            return false;
+        }
+
         LocalTime tradingStart = LocalTime.of(2, 0);
         LocalTime tradingEnd = LocalTime.of(20, 0);
 
